e265f59c15e122cbff91d0cac49703cdc0000648
cazabe <cazcarlos@hotmail.com>
2020-12-23 21:33:11 -0500

    Adding controller for company form and route

--- routes.js
+++ routes.js
@@ -7,11 +7,16 @@ const tagform = require('./controller/tagForm');
 
 //middleware with multer for uploading
 const uploadConfig = require("./config/upload");
+const tagFormEmp = require('./controller/tagFormEmp');
 const upload  = multer(uploadConfig);
 
 var cpUpload = upload.fields([{ name: 'fotocedula', maxCount: 2 }, { name: 'fototag', maxCount: 2 }])
 
-//form sending
+//form sending persona natural
 route.post("/form/create",cpUpload, tagform.registerTagAccount);
+
+//form sending empresas
+route.post("/form/create-empresa",cpUpload, tagFormEmp.registerTagAccountEmp);
+
 
 module.exports = route;
