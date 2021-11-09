09d32b7393d449f8b0e9c1513c0c62d5f2bdc32a
carlos zavala <cazcarlos@hotmail.com>
2021-11-09 15:32:36 -0500

    Testing

--- db_connection.js
+++ db_connection.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 const con = mysql.createConnection({
-  host: "localhost",
+  host: "localhost21",
   user: process.env.MYSQL_USER,
   password: "",
   port:process.env.MYSQL_PORT,
